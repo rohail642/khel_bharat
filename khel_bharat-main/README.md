6b956d8aeba1c66b24e1ba9a3516ffd299e04c3b
Rohail <rohail230@gmail.com>
2024-06-29 01:23:41 +0530

    new file:   khel_bharat-main/khel_Bharat copy/about.css 	new file:   khel_bharat-main/khel_Bharat copy/about.png 	new file:   khel_bharat-main/khel_Bharat copy/aboutus.css 	new file:   khel_bharat-main/khel_Bharat copy/card1.css 	new file:   khel_bharat-main/khel_Bharat copy/contactus.css 	new file:   khel_bharat-main/khel_Bharat copy/footer.css 	new file:   khel_bharat-main/khel_Bharat copy/style.css 	new file:   khel_bharat-main/khel_Bharat copy/try.html     commit

---
+++ khel_bharat-main/README.md
@@ -0,0 +1,2 @@
+Khel bharat is a platform to encorage and help sports people grow.
+Tech stack - HTML,CSS,JS, Python(Django),MySql
---
+++ khel_bharat-main/khel_Bharat/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.backgroundColor = "black";
+    const totalAnimationTime = (4) * 1000;
+    setTimeout(function() {
+        document.body.style.backgroundColor = "white";
+        document.querySelector('.main-loader').style.display = 'none';
+        // document.querySelector('.content').style.display = 'block';
+    }, totalAnimationTime);
+});
